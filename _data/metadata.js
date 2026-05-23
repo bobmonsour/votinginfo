fd1aff3bcad9dc390e682016693f27228bf68478
Bob Monsour <bob.monsour@gmail.com>
2026-05-23 09:01:32 -0700

    Title and description fix for home page

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 export default {
   title: "Election news, voter registration & voting requirements",
   description:
-    "Election news, along with a comprehensive guide to voter registration requirements for all 50 US states and Washington DC.",
+    "Election news, along with a comprehensive guide to voter registration and voting requirements for all 50 US states and Washington DC.",
   language: "en",
 };
